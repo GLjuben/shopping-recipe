bd5a9ed59e6ed01cf940df3df2ea7a63a38143dc
Gorjan Ljuben <gorjanljuben@gmail.com>
2021-11-07 14:30:56 +0100

    added dropdown service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,15 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A pizza recipe', 'This is a simple margarita pizza','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrQXJBVX7yq1JXXA7-uKCV7LxsQShngAlSFA&usqp=CAU'),
-    new Recipe('A pizza recipe', 'This is a simple margarita pizza','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrQXJBVX7yq1JXXA7-uKCV7LxsQShngAlSFA&usqp=CAU'),
-    new Recipe('A pizza recipe', 'This is a simple margarita pizza','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrQXJBVX7yq1JXXA7-uKCV7LxsQShngAlSFA&usqp=CAU')
+    new Recipe('An ice-cream recipe', 'This is vanila ice-cream','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrQXJBVX7yq1JXXA7-uKCV7LxsQShngAlSFA&usqp=CAU'),
+    new Recipe('A burger recipe', 'This is a delicious burger','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrQXJBVX7yq1JXXA7-uKCV7LxsQShngAlSFA&usqp=CAU')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
-  }
+  };
+  
+  onRecipeSelected(recipe:Recipe){
+   this.recipeWasSelected.emit(recipe)
+  };
 
 }
